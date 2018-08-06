c5cce3e0a8aa6c78fa2e8908d0359bb482d0ec66
Vytautas Gudzinevičius <gudzineviciusv@gmail.com>
2018-08-06 20:47:44 +0300

    Small changes according learning project

---
+++ blayer/src/components/UserInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const UserInput = () => {
+    return (
+        <div>
+            <input type="text" className="user-input"/>    
+         </div>
+    )
+}
+
+export default UserInput;
---
+++ blayer/src/components/UserOutput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const UserOutput = () => {
+    return (
+        <div>
+            <input type="text" className="user-input"/>    
+         </div>
+    )
+}
+
+export default UserOutput;
